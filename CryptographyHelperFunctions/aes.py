8b0d849886c2cbe53de62ef0ab0a242c10c8a9e3
John Luke <jldenny0207@aol.com>
2025-07-21 13:39:13 -0500

    finalized AES implementation

--- CryptographyHelperFunctions/aes.py
+++ CryptographyHelperFunctions/aes.py
@@ -68,4 +68,181 @@ inv_s_box = (
 )
 
 def sub_bytes(s: list[list[int]], sbox:tuple=s_box) -> list[list[int]]:
+    """Shifts the bytes of the current state by the S-Box
+
+    Args:
+        s (list[list[int]]): Current State
+        sbox (tuple, optional): The Shift matrix that defines how to shift the bytes of a state. Defaults to s_box.
+
+    Returns:
+        list[list[int]]: New state with shifted bytes
+    """
     return [[sbox[elem] for elem in row] for row in s]
+
+
+def shift_rows(s: list[list[int]]) -> None:
+    """Shifts the rows of a state
+
+    Args:
+        s (list[list[int]]): Current State
+    """
+    s[0][1], s[1][1], s[2][1], s[3][1] = s[1][1], s[2][1], s[3][1], s[0][1]
+    s[0][2], s[1][2], s[2][2], s[3][2] = s[2][2], s[3][2], s[0][2], s[1][2]
+    s[0][3], s[1][3], s[2][3], s[3][3] = s[3][3], s[0][3], s[1][3], s[2][3]
+
+
+def inv_shift_rows(s: list[list[int]]) -> None:
+    """Inverts the shift_rows function
+
+    Args:
+        s (list[list[int]]): current state
+    """
+    s[1][1], s[2][1], s[3][1], s[0][1] = s[0][1], s[1][1], s[2][1], s[3][1]
+    s[2][2], s[3][2], s[0][2], s[1][2] = s[0][2], s[1][2], s[2][2], s[3][2]
+    s[3][3], s[0][3], s[1][3], s[2][3] = s[0][3], s[1][3], s[2][3], s[3][3]
+
+
+# learned from http://cs.ucsb.edu/~koc/cs178/projects/JT/aes.c
+xtime = lambda a: (((a << 1) ^ 0x1B) & 0xFF) if (a & 0x80) else (a << 1)
+
+
+def mix_single_column(a: list[list[int]]) -> None:
+    """Mixes a single column for the AES implementation
+
+    Args:
+        a (list[list[int]]): column
+    """
+    # see Sec 4.1.2 in The Design of Rijndael
+    t = a[0] ^ a[1] ^ a[2] ^ a[3]
+    u = a[0]
+    a[0] ^= t ^ xtime(a[0] ^ a[1])
+    a[1] ^= t ^ xtime(a[1] ^ a[2])
+    a[2] ^= t ^ xtime(a[2] ^ a[3])
+    a[3] ^= t ^ xtime(a[3] ^ u)
+
+
+def mix_columns(s: list[list[int]]) -> None:
+    """Mixes the columns of the current state in accordance to AES
+
+    Args:
+        s (list[list[int]]): current state
+    """
+    for i in range(4):
+        mix_single_column(s[i])
+
+
+def inv_mix_columns(s: list[list[int]]) -> None:
+    """Inverts the mix_columns function
+
+    Args:
+        s (list[list[int]]): Current State
+    """
+    # see Sec 4.1.3 in The Design of Rijndael
+    for i in range(4):
+        u = xtime(xtime(s[i][0] ^ s[i][2]))
+        v = xtime(xtime(s[i][1] ^ s[i][3]))
+        s[i][0] ^= u
+        s[i][1] ^= v
+        s[i][2] ^= u
+        s[i][3] ^= v
+
+    mix_columns(s)
+
+N_ROUNDS = 10
+
+def expand_key(master_key: bytes) -> list[list[list[int]]]:
+    """
+    Expands and returns a list of key matrices for the given master_key.
+    """
+
+    # Round constants https://en.wikipedia.org/wiki/AES_key_schedule#Round_constants
+    r_con = (
+        0x00, 0x01, 0x02, 0x04, 0x08, 0x10, 0x20, 0x40,
+        0x80, 0x1B, 0x36, 0x6C, 0xD8, 0xAB, 0x4D, 0x9A,
+        0x2F, 0x5E, 0xBC, 0x63, 0xC6, 0x97, 0x35, 0x6A,
+        0xD4, 0xB3, 0x7D, 0xFA, 0xEF, 0xC5, 0x91, 0x39,
+    )
+
+    # Initialize round keys with raw key material.
+    key_columns = bytes2matrix(master_key)
+    iteration_size = len(master_key) // 4
+
+    # Each iteration has exactly as many columns as the key material.
+    i = 1
+    while len(key_columns) < (N_ROUNDS + 1) * 4:
+        # Copy previous word.
+        word = list(key_columns[-1])
+
+        # Perform schedule_core once every "row".
+        if len(key_columns) % iteration_size == 0:
+            # Circular shift.
+            word.append(word.pop(0))
+            # Map to S-BOX.
+            word = [s_box[b] for b in word]
+            # XOR with first byte of R-CON, since the others bytes of R-CON are 0.
+            word[0] ^= r_con[i]
+            i += 1
+        elif len(master_key) == 32 and len(key_columns) % iteration_size == 4:
+            # Run word through S-box in the fourth iteration when using a
+            # 256-bit key.
+            word = [s_box[b] for b in word]
+
+        # XOR with equivalent word from previous iteration.
+        word = bytes(i^j for i, j in zip(word, key_columns[-iteration_size]))
+        key_columns.append(word)
+
+    # Group key words in 4x4 byte matrices.
+    return [key_columns[4*i : 4*(i+1)] for i in range(len(key_columns) // 4)]
+
+
+def encrypt(key: bytes, plaintext: bytes) -> bytes:
+    round_keys = expand_key(key)
+
+    # Convert plaintext to state matrix
+    ciphertext = bytes2matrix(plaintext)
+
+    # Initial add round key step
+    ciphertext = add_round_key(ciphertext, round_keys[0])
+
+    for i in range(1, N_ROUNDS):
+        # Do round
+        ciphertext = sub_bytes(ciphertext, s_box)
+        shift_rows(ciphertext)
+        mix_columns(ciphertext)
+        ciphertext = add_round_key(ciphertext, round_keys[i])
+
+    # Run final round (skips the MixColumns step)
+    ciphertext = sub_bytes(ciphertext, s_box)
+    shift_rows(ciphertext)
+    ciphertext = add_round_key(ciphertext, round_keys[N_ROUNDS])
+
+    # Convert state matrix to ciphertext
+    ciphertext = matrix2bytes(ciphertext)
+
+    return ciphertext
+
+def decrypt(key: bytes, ciphertext: bytes) -> bytes:
+    round_keys = expand_key(key) # Remember to start from the last round key and work backwards through them when decrypting
+
+    # Convert ciphertext to state matrix
+    plaintext = bytes2matrix(ciphertext)
+
+    # Initial add round key step
+    plaintext = add_round_key(plaintext, round_keys[N_ROUNDS])
+
+    for i in range(N_ROUNDS - 1, 0, -1):
+        # Do round
+        inv_shift_rows(plaintext)
+        plaintext = sub_bytes(plaintext, inv_s_box)
+        plaintext = add_round_key(plaintext, round_keys[i])
+        inv_mix_columns(plaintext)
+
+    # Run final round (skips the InvMixColumns step)
+    inv_shift_rows(plaintext)
+    plaintext = sub_bytes(plaintext, inv_s_box)
+    plaintext = add_round_key(plaintext, round_keys[0])
+
+    # Convert state matrix to plaintext
+    plaintext = matrix2bytes(plaintext)
+
+    return plaintext
